18a5d3d2cefaeb89aabd0ba77faa76a4deea240e
AlexIsHappy <alexishappy@yandex.ru>
2015-05-15 14:50:18 +0200

    5/15/2015

---
+++ Buying a new car.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (int i in nbMonths(2000, 8000, 1000, 1.5))
+            {
+                Console.WriteLine(i);
+            }   
+            
+            // return  int[] r = new int[] { 6, 766 };
+            Console.ReadLine();
+        }
+
+        public static int[] nbMonths(int startPriceOld, int startPriceNew, int savingperMonth, double percentLossByMonth)
+        {
+            int sum = 0;
+            var oldCarPrice = Convert.ToDouble(startPriceOld);
+            var newCarPrice = Convert.ToDouble(startPriceNew);
+            int countM = 0;
+
+            while (newCarPrice > (sum + oldCarPrice))
+            {
+                sum += savingperMonth;
+                oldCarPrice = oldCarPrice - (oldCarPrice * percentLossByMonth) / 100;
+                newCarPrice = newCarPrice - (newCarPrice * percentLossByMonth) / 100;
+
+                if (countM % 2 == 0)
+                    percentLossByMonth += 0.5;
+
+                countM++;
+            }
+
+
+            int res = Convert.ToInt16(sum + oldCarPrice - newCarPrice);
+            return new int[] { countM, res};
+        }
+    }
+}
---
+++ PaskalTriangle.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(digPow(46288, 3));
+            Console.ReadLine();
+        }
+
+        public static long digPow(int n, int p)
+        {
+           int[] numbers = n.ToString().Select(x => Convert.ToInt32(x) - 48).ToArray();
+            var result = 0;
+            var count = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                result += Pow(numbers[count], p + i);
+                count++;
+            }
+
+            return result % n == 0 ? result / n : -1; 
+        }
+
+        public static int Pow(int num, int p)
+        {
+            int res = num;
+
+           while (p > 1)
+           {
+               res *= num;
+               p--;
+           }
+           return res;
+        }
+    }
+}
